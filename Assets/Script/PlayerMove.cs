a4e2275c2d59bddb0d4499369fd7d97ef3b6e36f
paigeberberick <paigeberberick7@gmail.com>
2026-09-14 22:35:06 -0400

    Steps 2 - 4 (Forgot to commit)

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -5,17 +5,18 @@ public class PlayerMove : MonoBehaviour
 { 
     [SerializeField] float moveSpeed = 5f; // Speed of the player movement
     [SerializeField] float rotateSpeed = 120f; // Speed of the player rotation
-    bool iskey = false;
+    bool hasPackage = false;
     SpriteRenderer PlayerRender;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PlayerRender = GetComponent<SpriteRenderer>();
+
+        Debug.Log("Player Sprite Renderer: " + PlayerRender);
     }
 
-        
-         
+
     // Update is called once per frame
     void Update()
     {
@@ -39,26 +40,40 @@ public class PlayerMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collision happened."+collision.gameObject.name);
-        if (collision.collider.CompareTag("Obstacle Square"));
-        {
-            Debug.Log("Hitting Obstacle");
+        Debug.Log("Collision happened." + collision.gameObject.name);
 
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("Grabbing Package");
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger happened");
-        if (other.CompareTag("HHH"))
+        // PICK UP PACKAGE
+        if (other.CompareTag("Package") && hasPackage == false)
         {
-            iskey = true;
-            Debug.Log("You triggered with an object." + other.gameObject.name);
-            Debug.Log(iskey);
+            hasPackage = true;
+
+            Debug.Log("Picked up the package.");
+
+            // Change Player color
             PlayerRender.color = Color.red;
-            Debug.Log(PlayerRender.color);
+
+            // Destroy the package
             Destroy(other.gameObject);
         }
-        
+
+        // DELIVER PACKAGE
+        if (other.CompareTag("Customer") && hasPackage == true)
+        {
+            hasPackage = false;
+
+            // Return Player to original color
+            PlayerRender.color = Color.white;
+
+            // Print delivery message
+            Debug.Log("Package delivered!");
+        }
     }
 }
